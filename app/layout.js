bb6d5bd23192f415fc0a2e26999e6ead3bd2ae14
Artimum <dorghamlli12@gmail.com>
2024-02-11 18:30:02 +0300

    new site

--- app/layout.js
+++ app/layout.js
@@ -1,31 +1,14 @@
 import './globals.css'
-import Nav from './Nav'
-import Header from './Header'
-import Home from './Home'
-import Script from 'next/script'
 
 export const metadata = {
   title: 'Artimum',
-  description: 'artimum is a full-stack web developer skilled in web development technologies',
+  description: 'i do web development',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      
-      <head>
-        <script src="https://cdn.jsdelivr.net/npm/baffle@0.3.6/dist/baffle.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/gh/hmongouachon/rgbKineticSlider/js/libs/TweenMax.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/gh/hmongouachon/rgbKineticSlider/js/libs/imagesLoaded.pkgd.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/gh/hmongouachon/rgbKineticSlider/js/libs/pixi.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/gh/hmongouachon/rgbKineticSlider/js/libs/pixi-filters.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/gh/hmongouachon/rgbKineticSlider/js/rgbKineticSlider.js"></script>
-      </head>
-
       <body>
-        <Nav />
-        <Header />
-        <Home />
         {children}
         </body>
     </html>
--- app/page.js
+++ app/page.js
@@ -1,7 +1,72 @@
+"use client"
+import Magnet from './magnet';
+import React, { useEffect } from 'react';
+
 export default function Main() {
+
+
+  useEffect(() => {
+    function boxFill(quantity) {
+      const container = document.getElementById("container");
+      container.innerHTML = '';
+
+      for (let i = 0; i < quantity; i++) {
+        let box = document.createElement("div");
+        box.classList.add("box");
+        container.appendChild(box);
+      }
+    }
+
+    function updateGrid() {
+      const newRows = Math.floor(document.body.clientHeight /40);
+      const newColumns = Math.floor(document.body.clientWidth /40);
+
+      const container = document.getElementById("container");
+      container.style.setProperty("--columns", newColumns);
+      container.style.setProperty("--rows", newRows);
+
+      boxFill(newRows * newColumns);
+    }
+
+    updateGrid();
+
+    window.addEventListener("resize", updateGrid);
+
+    return () => {
+      window.removeEventListener("resize", updateGrid);
+    };
+
+  }, []); 
+
   return (
-   <div>
-    <div className="pageTransition"></div>
+    <div>
+    <div id='container' className="container"></div>
+    <div className='nav'>
+      <Magnet>
+        <div className='expand'>
+           <a href='https://youtube.com/@artimum'>Youtube</a>
+        </div>
+      </Magnet>
+
+      <Magnet>
+        <div className='expand'>        
+        <a href='https://github.com/artimum1/'>Github</a> 
+        </div>
+      </Magnet>
+
+      <Magnet>
+      <div className='expand'>       
+       <a href='https://www.instagram.com/artimum1/'>Instagram</a> 
+      </div>
+
+      </Magnet>
+
+      <Magnet>
+      <div className='expand'>
+      <a href='https://discord.gg/c57Z8f8ec6'>Discord</a> 
+      </div>
+      </Magnet>
     </div>
-  )
+    </div>
+  );
 }
