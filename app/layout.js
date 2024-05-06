e40bd32b31e1244a221bd88e8e8bdda2fc8b2fb3
Artimum <dorghamlli12@gmail.com>
2024-05-06 22:04:19 +0300

    just an update

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <script src="https://cdn.jsdelivr.net/npm/poly-decomp@0.3.0/build/decomp.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/pathseg@1.2.1/pathseg.min.js"></script>
+      </head>
       <body>
         {children}
         </body>
--- app/page.js
+++ app/page.js
@@ -1,65 +1,103 @@
 "use client"
-import Magnet from './magnet';
-import React, { useEffect } from 'react';
+import Script from "next/script";
+import { useEffect } from "react";
+import Matter from "matter-js"
+export default function Home() {
+useEffect(()=>{
 
-export default function Main() {
 
+  
+  let container = document.getElementById("matter-container")
+  
+  
+      var Engine = Matter.Engine,
+          Render = Matter.Render,
+          Runner = Matter.Runner,
+          MouseConstraint = Matter.MouseConstraint,
+          Mouse = Matter.Mouse,
+          Composite = Matter.Composite,
+          Bodies = Matter.Bodies;
+  
+  
+  
+      // create engine
+      var engine = Engine.create(),
+          world = engine.world;
+  
+      // create renderer
+      var render = Render.create({
+          element: container,
+          engine: engine,
+          options: {
+              width: container.clientWidth,
+              height: container.clientHeight,
+              wireframes: false,
+              background: "#151515",
+          }
+      });
+  
+      Render.run(render);
+  
+      // create runner
+      var runner = Runner.create();
+      Runner.run(runner, engine);
+  let leftWall =  Bodies.rectangle(-25, container.clientHeight/2 , 50, container.clientHeight, { isStatic: true })
+  let ground = Bodies.rectangle(container.clientWidth/2, container.clientHeight+25 , container.clientWidth, 50, { isStatic: true })
+  let rightWall = Bodies.rectangle(container.clientWidth+50, container.clientHeight/2 , 100, container.clientHeight, { isStatic: true })
+  let roof =  Bodies.rectangle(container.clientWidth/2,-50 , container.clientWidth, 50, { isStatic: true })
 
-  useEffect(() => {
-    function boxFill(quantity) {
-      const container = document.getElementById("container");
-      container.innerHTML = '';
+      Composite.add(world, [
+          rightWall,ground,leftWall,roof
+      ]);
+  
 
-      for (let i = 0; i < quantity; i++) {
-        let box = document.createElement("div");
-        box.classList.add("box");
-        container.appendChild(box);
+      function createBalls(amount,radius){
+          for (let i = 0; i < amount; i++) {
+              Composite.add(engine.world,Bodies.circle((i*4), 0, radius,{restitution:1, friction:0.3,frictionAir:0.001}))
+          }
       }
-    }
+      createBalls(500,7)
+  
+      var mouse = Mouse.create(render.canvas),
+          mouseConstraint = MouseConstraint.create(engine, {
+              mouse: mouse,
+              constraint: {
+                  stiffness: 0.2,
+                  render: {
+                      visible: false
+                  }
+              }
+          });
+  
+      Composite.add(world, mouseConstraint);
+      
 
-    function updateGrid() {
-      const newRows = Math.floor(document.body.clientHeight /40);
-      const newColumns = Math.floor(document.body.clientWidth /40);
+window.addEventListener("resize",()=>{
+  render.canvas.width = container.clientWidth
+  render.canvas.height = container.clientHeight
 
-      const container = document.getElementById("container");
-      container.style.setProperty("--columns", newColumns);
-      container.style.setProperty("--rows", newRows);
 
-      boxFill(newRows * newColumns);
-    }
 
-    updateGrid();
+  Matter.Body.setPosition(
+  rightWall,
+  Matter.Vector.create(
+      container.clientWidth+50,
+      container.clientHeight/2
+  )
+  )
 
-    window.addEventListener("resize", updateGrid);
+})
 
-    return () => {
-      window.removeEventListener("resize", updateGrid);
-    };
+},[])
 
-  }, []); 
+
+
 
   return (
-    <div>
-    <div id='container' className="container"></div>
-    <div className='nav'>
-      <Magnet>
-        <div className='expand'>
-           <a target='_blank' href='https://youtube.com/@artimum'>Youtube</a>
-        </div>
-      </Magnet>
-
-      <Magnet>
-        <div className='expand'>        
-        <a target='_blank' href='https://github.com/artimum1/'>Github</a> 
-        </div>
-      </Magnet>
-
-      <Magnet>
-      <div className='expand'>
-      <a target='_blank' href='https://discord.gg/c57Z8f8ec6'>Discord</a> 
-      </div>
-      </Magnet>
-    </div>
-    </div>
+  <div>
+        <div className="header"><h1>Coming Soon</h1></div>
+        <div id="matter-container"></div>
+        <Script src="main.js" />
+  </div>
   );
 }
